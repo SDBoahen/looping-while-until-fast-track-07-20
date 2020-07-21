0369fea1acc9530041bc54496610c595331288c4
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-07-21 19:44:17 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,12 @@
 def using_until
   levitation_force = 6
+  
+  until 
+  puts "Wingardium Leviosa"
+  
+  levitation_force +=
   #your code here
     
 end
 
+"puts out the phrase 'Wingardium Leviosa' until the levitation force is incremented to 10"
